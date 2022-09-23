1524c31c110b5af8c428e685d16b49a3ad219d29
alexbreamdev <kortes39dev@gmail.com>
2022-09-23 22:04:57 +0300

    Initial Commit

---
+++ SwitUIToDoWithCoreData/SwitUIToDoWithCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitUIToDoWithCoreDataApp.swift
+//  SwitUIToDoWithCoreData
+//
+//  Created by Oleksii Leshchenko on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwitUIToDoWithCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
